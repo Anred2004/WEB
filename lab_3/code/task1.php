fb7c7d26c7e4c97eb646dfabd1cb78292491ca46
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2024-03-30 18:10:41 +0200

    added points a and in task1.php

--- lab_3/code/task1.php
+++ lab_3/code/task1.php
@@ -1,1 +1,15 @@
 <?php
+echo '<h1> Task №1 </h1>';
+$str = 'ahb acb aeb aeeb adcb axeb';
+preg_match_all('/a..b/', $str, $res);
+echo '<h3> A) </h3>';
+for ($i = 0; $i < sizeof($res[0]); $i++) {
+    echo "<p>{$res[0][$i]}</p>";
+}
+echo '<h3> B) </h3>';
+$str1 = 'a1b2c3';
+$str1 = preg_replace_callback('/\d+/', function($match){
+    return pow($match[0], 3);
+}, $str1);
+echo $str1;
+?>
